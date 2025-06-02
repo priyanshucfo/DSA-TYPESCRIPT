46ac0182b094e16e9a5f9a5104ccf1528ce03a50
= <leonmendonca619@gmail.com>
2025-06-02 19:09:49 +0530

    insertion sort

--- src/array/utils/index.ts
+++ src/array/utils/index.ts
@@ -4,6 +4,7 @@ import search from "./search.ts";
 import sort from "./sort.ts";
 
 const arr = [99, 98, 97, 96, 89, 1, 10, 90, 8, 67, 23, 54];
+const array100 = Array.from({ length: 20 }, () => Math.floor(Math.random() * 100) + 1);
 
 // traverse.arrayTraverse(arr, -1);
 
@@ -16,7 +17,7 @@ const arr = [99, 98, 97, 96, 89, 1, 10, 90, 8, 67, 23, 54];
 // const foundValue = search.binarySearch([2, 4, 6, 8, 10, 12, 14, 16, 18], 12);
 // console.log(foundValue);
 
-const sortedArray = sort.selectionSort(arr);
+const sortedArray = sort.insertionSort(arr);
 console.log(sortedArray);
 
 // console.log("Array", arr);
--- src/array/utils/sort.ts
+++ src/array/utils/sort.ts
@@ -19,6 +19,7 @@ function selectionSort<T>(array: T[]) {
         smIdx = j;
       }
     }
+    
     if (array[i] > array[smIdx]) {
       let temp = array[i];
       array[i] = array[smIdx];
@@ -28,6 +29,25 @@ function selectionSort<T>(array: T[]) {
   return array;
 }
 
-function insertionSort<T>(array: T[]) {}
+function insertionSort<T>(array: T[]) {
+  for (let i = 1; i < array.length; i++) {
+    let key = i;
+    let keyValue = array[i];
+    let j = 0;
+    for (j = key - 1; j >= 0; j--) {
+      // console.log("Key is", keyValue, "Elements before key", array[j]);
+      if (array[j] > keyValue) {
+        // console.log("before", array);
+        array[j + 1] = array[j];
+        // console.log("after", array);
+      } else {
+        break;
+      }
+    }
+    array[j + 1] = keyValue;
+    // console.log("Array after iteration", array);
+  }
+  return array;
+}
 
 export default { bubbleSort, selectionSort, insertionSort };
